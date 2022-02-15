627dcb67fa32e0f0358cfa9c0814e71fbe95a787
amh6281 <amh6281@naver.com>
2022-02-15 16:12:36 +0900

    Total 가격 에러 해결 2022.02.15
    가격을 price="10000" 이란 잘못된 형식으로 인해 Total 가격에 에러가 생겼다.
    price={10000} 이란 형식으로 에러를 해결한다.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,21 +16,21 @@ function Home() {
               <Product
                 id="1"
                 title="제품 1"
-                price="30,000"
+                price={30000}
                 image="https://imgc.1300k.com/aaaaaib/goods/215024/46/215024460467.jpg?3"
                 rating={5}
               />
               <Product
                 id="2"
                 title="제품 2"
-                price="22,000"
+                price={22000}
                 image="http://image.auction.co.kr/itemimage/1a/de/08/1ade0827d6.jpg"
                 rating={5}
               />
               <Product
                 id="3"
                 title="제품 3"
-                price="19,000"
+                price={19000}
                 image="https://shop4.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop4.daumcdn.net%2Fshophow%2Fp%2FB5101293117.jpg%3Fut%3D20210116072114"
                 rating={5}
               />
@@ -39,21 +39,21 @@ function Home() {
               <Product
                 id="4"
                 title="제품 4"
-                price="29,000"
+                price={29000}
                 image="https://m.ontop.kr/web/product/big/202111/5926aa58b4152d6b6b4c7d27d1e24e76.jpg"
                 rating={5}
               />
               <Product
                 id="5"
                 title="제품 5"
-                price="32,000"
+                price={32000}
                 image="http://img.danawa.com/prod_img/500000/314/010/img/7010314_1.jpg?shrink=330:330&_v=20190110114756"
                 rating={5}
               />
               <Product
                 id="6"
                 title="제품 6"
-                price="28,500"
+                price={28500}
                 image="http://image.kyobobook.co.kr/newimages/giftshop_new/goods/400/1436/hot1613611465747.jpg"
                 rating={5}
               />
@@ -62,21 +62,21 @@ function Home() {
               <Product
                 id="7"
                 title="제품 7"
-                price="34,500"
+                price={34500}
                 image="https://imgc.1300k.com/aaaaaib/goods/215024/87/215024872385.jpg?3"
                 rating={5}
               />
               <Product
                 id="8"
                 title="제품 8"
-                price="22,500"
+                price={22500}
                 image="http://s2pink.com/web/product/big/202009/b126e0dba3795b987f98b5e0a2e60ca7.jpg"
                 rating={5}
               />
               <Product
                 id="9"
                 title="제품 9"
-                price="12,000"
+                price={12000}
                 image="https://image.msscdn.net/images/goods_img/20210826/2090180/2090180_1_500.jpg"
                 rating={5}
               />
